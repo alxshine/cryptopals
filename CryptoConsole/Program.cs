7c92bdbdbb82c2e0e10c7e22825c36a8f5c90dd0
alxshine <alexander.schloegl@student.uibk.ac.at>
2018-01-17 14:26:04 +0100

    Add project files.

---
+++ CryptoConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CryptoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            var target = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+            var converted = Utility.ByteArrayFromHexString(input);
+            var convertedString = Utility.HexStringFromBinaryArray(converted);
+            var correct = input.CompareTo(convertedString) == 0;
+
+            Console.WriteLine("input: {0}", input);
+            Console.WriteLine("converted: {0:x}", convertedString);
+            Console.WriteLine("Correct: {0}", correct);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CryptoConsole/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoConsole
+{
+    abstract class Utility
+    {
+        public static byte[] ByteArrayFromHexString(String source)
+        {
+            if (source.Length % 2 == 1)
+            {
+                source = "0" + source;
+            }
+
+            byte[] targetArray = new byte[source.Length / 2];
+
+            for (var i = 0; i < targetArray.Length; i++)
+            {
+                targetArray[i] = Convert.ToByte(source.Substring(2 * i, 2), 16);
+            }
+
+            return targetArray;
+        }
+
+        public static String HexStringFromBinaryArray(byte[] source)
+        {
+            var output = new StringBuilder();
+            foreach (var b in source)
+            {
+                output.Append(b.ToString("x"));
+            }
+            return output.ToString();
+        }
+
+        public static String ConvertBinaryArrayToBase64(byte[] source)
+        {
+            return Convert.ToBase64String(source);
+        }
+    }
+}
